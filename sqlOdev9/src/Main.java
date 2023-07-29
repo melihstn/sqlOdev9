96b7917b2975a76e071d6218b1d4bf6c904209c3
melihstn <130406104+melihstn@users.noreply.github.com>
2023-07-29 16:39:50 +0300

    Add files via upload

---
+++ sqlOdev9/src/Main.java
@@ -0,0 +1,14 @@
+/*
+
+1)
+    select city,country from CITY
+    inner join COUNTRY on city.country_id = country.country_id;
+
+2)
+    select payment_id, first_name, last_name FROM customer
+    INNER JOIN payment ON customer.customer_id = payment.customer_id;
+3)
+    SELECT rental_id, first_name, last_name FROM customer
+    INNER JOIN rental ON customer.customer_id = rental.customer_id;
+
+ */
